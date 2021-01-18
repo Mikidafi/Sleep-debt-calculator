8837a1665ef55361f31ed3712f8aa23f502d6f8c
mikidafi <mchl.str84@gmail.com>
2021-01-18 22:12:03 +0100

    Prvy commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// This program takes user input for hours slept by day of the week and compares these to user-provided ideal sleep hours
+// Weekly sleep debt or surplus is returned in the case that sleep is more or less than ideal
+
+
+function getHours(day) {
+    const hours = prompt(`How many hours this you get on ${day} ?`) 
+    return Number(hours);
+}
+
+function calculation() {
+    var sum = 0;
+    sum += getHours('Monday');
+    sum += getHours('Yuesday');
+    sum += getHours('Wednesday');
+    sum += getHours('Thursday');
+    sum += getHours('Friday');
+    sum += getHours('Saturday');
+    sum += getHours('Sunday');
+    return sum;
+}
+
+function idealHours() {
+    var ideal = prompt("How many hours would you like to sleep a day?")
+    return ideal * 7;
+}
+
+function finalCalc() {
+    var planned = idealHours();
+    var slept = calculation();
+    if ( planned > slept ) {
+        var debt = 0;
+        debt = planned - slept;
+        console.log(`You slept ${debt} hours less than you planned.`)
+
+    }else if( planned < slept ){
+        var debt = 0;
+        debt = slept - planned;
+        console.log(`You slept ${debt} hours more than you planned.`)
+    }else if ( planned === slept ) {
+        console.log("You achieved your sleep goal!")
+        }else{
+            console.log("Erorr")
+        }
+    console.log(`planned ${planned}`);
+    console.log(`slept ${slept}`);
+}
+finalCalc();
